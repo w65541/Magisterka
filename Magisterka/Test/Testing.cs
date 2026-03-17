197af2b3c602c5280a26ff04e14599b5b95643a2
w65541 <w65541@student.wsiz.edu.pl>
2026-03-17 16:49:47 +0100

    automated testing

--- Magisterka/Test/Testing.cs
+++ Magisterka/Test/Testing.cs
@@ -1,10 +1,13 @@
-﻿using System.Diagnostics;
+﻿using Magisterka.CSV;
+using Magisterka.Data;
+using Magisterka.Parquet;
+using System.Diagnostics;
 
 namespace Magisterka.Test
 {
     public class Testing
     {
-        public static BenchmarkResult RunTest<T>(List<T> data,string path,Action<string, IEnumerable<T>> write,Func<string, List<T>> read)
+        public static BenchmarkResult RunTest<T>(List<T> data, string path, Action<string, IEnumerable<T>> write, Func<string, List<T>> read)
         {
             var result = new BenchmarkResult();
 
@@ -37,7 +40,7 @@ namespace Magisterka.Test
             return result;
         }
 
-        public static async Task<BenchmarkResult> RunTestAsync<T>(List<T> data,string path,Func<string, IEnumerable<T>, Task> write,Func<string, Task<List<T>>> read)
+        public static async Task<BenchmarkResult> RunTestAsync<T>(List<T> data, string path, Func<string, IEnumerable<T>, Task> write, Func<string, Task<List<T>>> read)
         {
             var result = new BenchmarkResult();
 
@@ -68,6 +71,61 @@ namespace Magisterka.Test
             result.ReadThroughput = fileSizeMB / (result.ReadTimeMs / 1000.0);
 
             return result;
+        }
+        public static async void runXcsvAsync<T>(List<T> data, string path, int x) where T : class, new()
+        {
+            //Warm up
+            for (int i = 0; i < 15; i++)
+            {
+                BenchmarkResult csvResult = await Testing.RunTestAsync(
+                data,
+                path,
+                TestParquet.WriteParquet,
+                TestParquet.ReadParquet<T>
+            );
+            }
+                List<BenchmarkResult> results = new List<BenchmarkResult>();
+                for (int i = 0; i < x; i++)
+                {
+                    BenchmarkResult csvResult = await Testing.RunTestAsync(
+                    data,
+                    path,
+                    TestParquet.WriteParquet,
+                    TestParquet.ReadParquet<T>
+                );
+                    results.Add(csvResult);
+                }
+                TestCsv.WriteCsv("parquetResult" + path + ".csv", results);
+
+
+        }
+        public static void runXcsv<T>(List<T> data, string path, int x)
+        {
+            //Warm up
+            for (int i = 0; i < 15; i++)
+            {
+                BenchmarkResult csvResult = Testing.RunTest(
+                data,
+                path,
+                TestCsv.WriteCsv,
+                 TestCsv.ReadCsv<T>
+            );
+
+
+                List<BenchmarkResult> results = new List<BenchmarkResult>();
+                for (i = 0; i < x; i++)
+                {
+                    csvResult = Testing.RunTest(
+                    data,
+                    path,
+                    TestCsv.WriteCsv,
+                     TestCsv.ReadCsv<T>
+                );
+                    results.Add(csvResult);
+                }
+                TestCsv.WriteCsv("csvResult" + path + ".csv", results);
+            }
+
         }
     }
 }
